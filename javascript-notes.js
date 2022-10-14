f21e3574c4992353ed9ed5a82c35321b0988bc18
Ben Daniels <benjamindaniels21@gmail.com>
2022-10-14 19:41:54 -0400

    the THIS keyword

--- javascript-notes.js
+++ javascript-notes.js
@@ -4,7 +4,7 @@
 
 -----Add methods to object-----
 -You can add functions to an object that you can later tap into using dot notation 
--like this: 
+like this: 
 
 
 const myObject = {
@@ -17,7 +17,7 @@ const myObject = {
 
 myObject.sayWhatsUp();
 
-However, this is a little cluttered, so we can make it even easier, and drop the colon 
+-However, this is a little cluttered, so we can make it even easier, and drop the colon 
 and just write a plain function name which will take the place of the key. Like this:
 
 const myNewObject = {
@@ -36,6 +36,31 @@ Just make sure that you add a comma after the method, so you don't get an error!
 -It’s a keyword 
 -depending on the scope of  the returned ‘this’ object may change. 
 -using 
+-If the "this" keyword is used just in a plain ol' function it's going to return the window
+object 
+    For example: 
+    function myBooty(){
+        console.log(this)
+    }
+-Here, 'this' returns the window, because it doesn't live inside an object.
+-If 'this' lives inside a method (i.e.) inside an object we define, then we get the object back
+    For Example: 
+
+
+const person = {
+  first: "Bill",
+  last: "Daniels",
+  nickName: "Barky Barker",
+  cryForFood() {
+    console.log(this);
+  },
+};
+
+person.cryForFood();
+
+
+-If the "this"
+
 
 -----The Window-----
 -This is the global object 
