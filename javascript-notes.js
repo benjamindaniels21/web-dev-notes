271e57ad579fe5190870f87f3b4ca03dd77bfbbc
Ben Daniels <benjamindaniels21@gmail.com>
2023-01-26 08:28:19 -0500

    getters and setters

--- javascript-notes.js
+++ javascript-notes.js
@@ -938,14 +938,16 @@ class Cat extends Pet{
 To determine value of 'this' 
 
 Did you use an arrow function --> write a console.log on the first valid line above the arrow function. Value of 'this' in the arrow function will be equal to that console log
-
+    -will result in the window object usually 
 Dig you call 'bind', 'call', or 'apply' on the function when you invoked it? --> 'this' is equal to the first argument of 'bind', 'call', or 'apply'
 
 All other cases --> 'this' is equal to whatever is to the left of the '.' in the method call. 
 
 
-
-
+*******************************
+--getters and setters---
+*******************************
+I'm confused about this part...look into it more. 
 
 bump
 */
