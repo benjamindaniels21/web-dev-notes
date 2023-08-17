41f6e126fde869b5d295816c7dce8dfd02856137
Ben Daniels <benjamindaniels21@gmail.com>
2023-08-17 18:23:03 -0400

    create linked list with methods

--- javascript-notes.js
+++ javascript-notes.js
@@ -1869,7 +1869,71 @@ Example: 4 -> 5 -> 6 -> null
 -next is also a pointer
 -Random access is not allowed!
 
+Creating a Linked List with different methods: 
 
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+
+class SinglyLinkedList{
+    constructor(){
+        this.length = 0;
+        this.head = null;
+        this.tail = null;
+    }
+    push(val){
+      var newNode = new Node(val);
+        
+      if (!this.head){
+          this.head = newNode;
+          this.tail = this.head;
+      } else {
+          this.tail.next = newNode;
+          this.tail = newNode;
+      }
+        this.length++;
+        return this;
+      
+    }
+    pop(){
+    if (!this.head) return undefined;
+    let current = this.head;  
+    let newTail = current;   
+   
+    while(current.next){
+       newTail = current; 
+       current = current.next;
+         
+    }
+      this.tail = newTail;
+      this.tail.next = null;
+      this.length--;
+        if(this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+      return current;
+    }
+    shift(){
+        if(!this.head){
+            return undefined;
+        } 
+        let currentHead = this.head; 
+        this.head = currentHead.next;
+        this.length--;
+        return currentHead;
+    }
+}
+
+
+let list = new SinglyLinkedList()
+list.push("hello");
+list.push("there");
+list.push("friend");
 
 
 -This data type has a value and a pointer 
